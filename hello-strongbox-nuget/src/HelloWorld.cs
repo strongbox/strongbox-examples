a61adec7179b7327d52b6a1d1e3db46e7dc7d23d
sbespalov <serge.bespalov@gmail.com>
2016-11-03 10:21:52 +0600

    Strongbox example with Nuget.

---
+++ hello-strongbox-nuget/src/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace org.carlspring.strongbox.test.nuget
+{
+    public class HelloWorld
+	{
+		static public void Main ()
+		{
+			Console.WriteLine ("Hello Mono World");
+		}
+    }               
+}
